6b8df0020533bdab1e1d6a6abec368d1264b7fbf
hieugithub2 <hieumessideptrai@gmail.com>
2023-05-30 23:02:56 -0400

    add test case, time, memory usage

--- Leo/Stack.cpp
+++ Leo/Stack.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <chrono>
+#include <memory>
 using namespace std;
 
 class Node {
@@ -16,7 +17,6 @@ class Stack {
         void push(char data);
         char pop();
         char display();
-        int capacity;
     private:
         Node *head;
 };
@@ -27,7 +27,7 @@ Stack::Stack(){
 };
 
 void Stack::push(char data){
-    Node *new_node = new Node();
+    Node *new_node = new Node;
     new_node->data = data;
     new_node->next = NULL;
     
@@ -92,6 +92,8 @@ int TestStack(string string_test){
 
 
 int main(){
+    auto start = std::chrono::steady_clock::now();
+
     string test1 = "{{{{}}}[][]((())";    // * False
     string test2 = "(%TT{>><]]]%%%%%%)";  // * False
     string test3 = "(((((()))))){}{}{}[][][][[[]]]"; // * True
@@ -102,7 +104,28 @@ int main(){
     string test8 = "((((((}}}}}}"; // * False
     string test9 = "Aaaaaaaa+bbbb({}{}{}[][][]tttttt + %^$)"; // * True
     string test10 = "@@@@@@@@@@@@@@@@@@@@@@@$$%%T$%$%$$%{}()%%%%%%%%%%%%%%%%%%%%%%%%%%%% "; // * True
+    string test11 = "<<<<<{{{{[@@@@((((%%%%%%............))))]]]*****######!!!!!!!}}}}>>>>>"; // * False
+    string test12 = "(((()()()())[{}]))[[[]]]{[[()()][]]}"; // * True
+    string test13 = "(((([[{{((()))}}]]]))))([{<>}])"; // * False
+    string test14 = "((((({[((([])))][{(())}]})}))))"; // * False
+    string test15 = "((((({[2312((123([2131])*))][{(())}]})}))))"; // * False
+    string test16 = "((((({[2312((123([2131])*))][{(())}]}2312((123([2131"; // * False
     
-    cout << TestStack(test10) << endl;
+    // cout << TestStack(test10) << endl;
+    // cout << TestStack(test11) << endl;
+    // cout << TestStack(test12) << endl;
+    // cout << TestStack(test13) << endl;
+    // cout << TestStack(test14) << endl;
+
+    cout << "Result for test case 15: ";
+    cout << TestStack(test15) << endl;
+
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Execution time: " << duration.count() << " us (microsecond)" << std::endl;
+
+    // get memory consumption
+    std::unique_ptr<int[]> ptr(new int[1024 * 1024]);
+    std::cout << "Memory usage: " << 1024 * 1024 * sizeof(int) << " bytes" << std::endl;
     return 0;
 }
